8afc297557f6b90754034bf01f5543f982cc71a5
Powerdriven <powerdriven@gmail.com>
2017-08-21 15:06:06 -0500

    Tutorial 3 complete

--- screeps.com/tutorial-2/role.upgrader.js
+++ screeps.com/tutorial-2/role.upgrader.js
@@ -1,27 +1,4 @@
-/*var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-
-module.exports.loop = function () {
-
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('role.upgrader');
- * mod.thing == 'a thing'; // true
- */
-
-module.exports = {
-
-};var roleUpgrader = {
+var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
